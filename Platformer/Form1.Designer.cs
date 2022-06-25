f843ce182f51cdc6325a163daa634ab4ffc9d80c
Julian Gabriel <juliangabriel570@gmail.com>
2022-06-25 23:27:16 +0200

    Projektdateien hinzufügen.

---
+++ Platformer/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Platformer
+{
+    partial class Platformer
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbGround = new System.Windows.Forms.PictureBox();
+            this.pbPlayer = new System.Windows.Forms.PictureBox();
+            this.tmrGravity = new System.Windows.Forms.Timer(this.components);
+            this.tmrUp = new System.Windows.Forms.Timer(this.components);
+            this.tmrRight = new System.Windows.Forms.Timer(this.components);
+            this.tmrLeft = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGround
+            // 
+            this.pbGround.BackColor = System.Drawing.Color.DarkGreen;
+            this.pbGround.Location = new System.Drawing.Point(-28, 661);
+            this.pbGround.Name = "pbGround";
+            this.pbGround.Size = new System.Drawing.Size(878, 75);
+            this.pbGround.TabIndex = 0;
+            this.pbGround.TabStop = false;
+            // 
+            // pbPlayer
+            // 
+            this.pbPlayer.BackColor = System.Drawing.Color.Black;
+            this.pbPlayer.Location = new System.Drawing.Point(322, 302);
+            this.pbPlayer.Name = "pbPlayer";
+            this.pbPlayer.Size = new System.Drawing.Size(50, 50);
+            this.pbPlayer.TabIndex = 1;
+            this.pbPlayer.TabStop = false;
+            // 
+            // tmrGravity
+            // 
+            this.tmrGravity.Enabled = true;
+            this.tmrGravity.Interval = 10;
+            this.tmrGravity.Tick += new System.EventHandler(this.tmrGravity_Tick);
+            // 
+            // tmrUp
+            // 
+            this.tmrUp.Interval = 10;
+            this.tmrUp.Tick += new System.EventHandler(this.tmrUp_Tick);
+            // 
+            // tmrRight
+            // 
+            this.tmrRight.Interval = 10;
+            this.tmrRight.Tick += new System.EventHandler(this.tmrRight_Tick);
+            // 
+            // tmrLeft
+            // 
+            this.tmrLeft.Interval = 10;
+            this.tmrLeft.Tick += new System.EventHandler(this.tmrLeft_Tick);
+            // 
+            // Platformer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(772, 724);
+            this.Controls.Add(this.pbPlayer);
+            this.Controls.Add(this.pbGround);
+            this.Name = "Platformer";
+            this.Text = "Platformer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Platformer_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Platformer_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGround;
+        private System.Windows.Forms.PictureBox pbPlayer;
+        private System.Windows.Forms.Timer tmrGravity;
+        private System.Windows.Forms.Timer tmrUp;
+        private System.Windows.Forms.Timer tmrRight;
+        private System.Windows.Forms.Timer tmrLeft;
+    }
+}
+
---
+++ Platformer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platformer
+{
+    public partial class Platformer : Form
+    {
+        bool isJumping = false;
+
+        public Platformer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrGravity_Tick(object sender, EventArgs e)
+        {
+            if(!pbPlayer.Bounds.IntersectsWith(pbGround.Bounds) && isJumping == false)
+            {
+                pbPlayer.Top += 10; 
+            }
+        }
+
+        private void tmrLeft_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Left -= 10;
+        }
+
+        private void tmrUp_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Top -= 10;
+            isJumping = true;
+        }
+
+        private void tmrRight_Tick(object sender, EventArgs e)
+        {
+            pbPlayer.Left += 10;
+        }
+
+        private void Platformer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tmrUp.Start();
+            } else if (e.KeyCode == Keys.Right)
+            {
+                tmrRight.Start();
+            } else if (e.KeyCode == Keys.Left)
+            {
+                tmrLeft.Start();
+            }
+        }
+
+        private void Platformer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tmrUp.Stop();
+                isJumping = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                tmrRight.Stop();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                tmrLeft.Stop();
+            }
+        }
+    }
+    
+}
